b8b089b485bd4c963c95fc07d6fa3cdfdcab2d30
Lakshmi Priya <lakshmiraj070@gmail.com>
2026-07-20 09:31:54 +0530

    Add project files.

---
+++ Models/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Models
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
